49907c5fe67f76cfbad1abff631d9da9a5b54573
Toka <tokahossam806@gmail.com>
2026-02-08 11:51:59 +0200

    update

--- dcm4chee-viewer/tailwind.config.js
+++ dcm4chee-viewer/tailwind.config.js
@@ -4,28 +4,17 @@ export default {
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-<<<<<<< HEAD
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['var(system-ui, Avenir, Helvetica, Arial)', 'sans-serif'],
-=======
-
   theme: {
     extend: {
       fontFamily: {
         sans: ['var( system-ui, Avenir, Helvetica, Arial)', 'sans-serif'],
->>>>>>> 108c9682e78efed61378baff8c32cc233043cc25
         montserrat: ['Montserrat', 'sans-serif'],
         lato: ['Lato', 'sans-serif'],
       },
     },
   },
   plugins: [],
-<<<<<<< HEAD
 };
-=======
-}
 
 // module.exports = {
 //   theme: {
@@ -37,4 +26,3 @@ export default {
 //     },
 //   },
 // }
->>>>>>> 108c9682e78efed61378baff8c32cc233043cc25
